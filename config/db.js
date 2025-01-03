c499f4cb0e90b91044d177b9785d8de75ac71e5e
poornima0227 <poornimalakshmi807@gmail.com.com>
2025-01-03 19:24:23 +0530

    Updated db.js  for .env support and improved error handling

--- config/db.js
+++ config/db.js
@@ -3,10 +3,10 @@ require('dotenv').config(); // Load environment variables from .env file
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'ams',
+  host: process.env.DB_HOST ,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
